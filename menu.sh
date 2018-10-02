dae77fc8456202b29c49b59d44cc6b720b254b00
roberthampton <37349210+roberthampton@users.noreply.github.com>
2018-10-02 16:42:29 +0100

    Add files via upload

---
+++ menu.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#This is a draft of the menu to be edited and updated later, once we know exactly what the options are
+
+echo 'Please enter your choice: '
+options=("Option 1" "Option 2" "Option 3" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Option 1")
+            echo "you chose choice 1"
+            ;;
+        "Option 2")
+            echo "you chose choice 2"
+            ;;
+        "Option 3")
+            echo "you chose choice 3"
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
